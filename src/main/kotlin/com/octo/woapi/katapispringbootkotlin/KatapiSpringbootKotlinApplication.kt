400d2d40d166dc4737a6783526d7570a3332f795
Kévin JULIEN <kevin.julien@octo.com>
2021-03-15 18:02:22 +0100

    build: génération du socle via spring initializer

---
+++ src/main/kotlin/com/octo/woapi/katapispringbootkotlin/KatapiSpringbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.octo.woapi.katapispringbootkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KatapiSpringbootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<KatapiSpringbootKotlinApplication>(*args)
+}
