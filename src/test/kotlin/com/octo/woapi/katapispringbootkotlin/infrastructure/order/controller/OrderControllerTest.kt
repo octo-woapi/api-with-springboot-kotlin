534f8be5aa12e1eb5c66b3836067579337378156
Kévin JULIEN <kevin.julien@octo.com>
2021-04-14 17:55:02 +0200

    feat: ajout de la route de création et de suppression des orders

--- src/test/kotlin/com/octo/woapi/katapispringbootkotlin/infrastructure/order/controller/OrderControllerTest.kt
+++ src/test/kotlin/com/octo/woapi/katapispringbootkotlin/infrastructure/order/controller/OrderControllerTest.kt
@@ -2,31 +2,28 @@ package com.octo.woapi.katapispringbootkotlin.infrastructure.order.controller
 
 import com.octo.woapi.katapispringbootkotlin.infrastructure.order.exception.OrderNotFoundException
 import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.hasSize
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.transaction.annotation.Transactional
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import javax.transaction.Transactional
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-class OrderControllerTest {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+class OrderControllerTest(@Autowired val mockMvc: MockMvc) {
 
     @Nested
     inner class GetAllOrders {
@@ -84,6 +81,101 @@ class OrderControllerTest {
                     MockMvcResultMatchers.content()
                         .string(Matchers.containsString("could not find order with id : $idToBeTested"))
                 )
+        }
+    }
+
+    @Nested
+    inner class CreateOrder {
+        @Test
+        fun `should return the new order with a new id`() {
+            //given
+            @Language(value = "json")
+            val productsJson = """
+            [
+                {
+                    "name" : "a-product",
+                    "price": 10.10,
+                    "weight": 5
+                },
+                {
+                    "name" : "another-product",
+                    "price": 200.50,
+                    "weight": 50
+                }
+            
+            ]          
+        """.trimIndent()
+            //when-then
+            mockMvc.perform(
+                MockMvcRequestBuilders.post("/orders")
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(productsJson)
+            )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated)
+                .andExpect(jsonPath("$.id", `is`(Matchers.notNullValue())))
+                .andExpect(jsonPath("$.status", `is`("PENDING")))
+                .andExpect(jsonPath("$.products", hasSize<Int>(2)))
+                .andExpect(jsonPath("$.totalWeight", `is`(55)))
+                .andExpect(jsonPath("$.totalAmount", `is`(335.6)))
+                .andExpect(jsonPath("$.shipmentAmount", `is`(125)))
+        }
+
+        @Test
+        fun `should return http code 400 if no product`() {
+            //given
+            @Language(value = "json")
+            val productJson = """
+                []                      
+        """.trimIndent()
+            //when-then
+            mockMvc.perform(
+                MockMvcRequestBuilders.post("/orders")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(productJson)
+            )
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError)
+        }
+
+        @Test
+        fun `should return http code 400 if a product is incorrect`() {
+            //given
+            @Language(value = "json")
+            val productJson = """
+                [
+                    {
+                    "price": 10.10,
+                    "weight": 12.34
+                    } 
+                ]                      
+        """.trimIndent()
+            //when-then
+            mockMvc.perform(
+                MockMvcRequestBuilders.post("/orders")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(productJson)
+            )
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError)
+                .andExpect(jsonPath("$.detail", `is`("param 'name' is required")))
+        }
+    }
+
+    @Nested
+    inner class DeleteOrder {
+        @Test
+        fun `should delete an order and return http code 204`() {
+            val idToBeTested = 2
+            mockMvc.perform(MockMvcRequestBuilders.delete("/orders/$idToBeTested"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent)
+        }
+
+        @Test
+        fun `should return http code 404 if order not found`() {
+            val idToBeTested = 5555
+            mockMvc.perform(MockMvcRequestBuilders.delete("/orders/$idToBeTested"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+                .andExpect(jsonPath("$.detail", `is`("could not find order with id : $idToBeTested")))
         }
     }
 
--- src/test/kotlin/com/octo/woapi/katapispringbootkotlin/infrastructure/order/persistence/OrderDBServiceImplTest.kt
+++ src/test/kotlin/com/octo/woapi/katapispringbootkotlin/infrastructure/order/persistence/OrderDBServiceImplTest.kt
@@ -3,11 +3,12 @@ package com.octo.woapi.katapispringbootkotlin.infrastructure.order.persistence
 import com.octo.woapi.katapispringbootkotlin.domain.order.OrderStatus
 import com.octo.woapi.katapispringbootkotlin.domain.product.Product
 import com.octo.woapi.katapispringbootkotlin.infrastructure.order.exception.OrderNotFoundException
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.transaction.annotation.Transactional
+import javax.transaction.Transactional
 
 @SpringBootTest
 @Transactional
@@ -18,7 +19,7 @@ class OrderDBServiceImplTest(@Autowired val orderDBServiceImpl: OrderDBServiceIm
         //when
         val orders = orderDBServiceImpl.getAllOrders()
         //then
-        Assertions.assertThat(orders).isNotNull.hasSize(2)
+        assertThat(orders).isNotNull.hasSize(2)
     }
 
     @Test
@@ -26,15 +27,16 @@ class OrderDBServiceImplTest(@Autowired val orderDBServiceImpl: OrderDBServiceIm
         //when
         val anOrder = orderDBServiceImpl.getOrderById(1)
         //then
-        Assertions.assertThat(anOrder).isNotNull
+        assertThat(anOrder).isNotNull
             .hasFieldOrPropertyWithValue("id", 1L)
             .hasFieldOrPropertyWithValue("status", OrderStatus.PENDING)
+        assertThat(anOrder.products).isNotNull.hasSize(1)
     }
 
     @Test
     fun `getOrderById should throw an exception with giving an incorrect id`() {
         //when-then
-        Assertions.assertThatThrownBy { orderDBServiceImpl.getOrderById(999) }
+        assertThatThrownBy { orderDBServiceImpl.getOrderById(999) }
             .isInstanceOf(OrderNotFoundException::class.java)
     }
 
@@ -44,26 +46,27 @@ class OrderDBServiceImplTest(@Autowired val orderDBServiceImpl: OrderDBServiceIm
         orderDBServiceImpl.deleteOrder(1)
         val orders = orderDBServiceImpl.getAllOrders()
         //then
-        Assertions.assertThat(orders).hasSize(1)
+        assertThat(orders).hasSize(1)
     }
 
     @Test
     fun `deleteOrderById should delete 0 order, throw an exception when giving an incorrect id`() {
         //when-then
-        Assertions.assertThatThrownBy { orderDBServiceImpl.deleteOrder(999) }
+        assertThatThrownBy { orderDBServiceImpl.deleteOrder(999) }
             .isInstanceOf(OrderNotFoundException::class.java)
     }
 
     @Test
-    fun `createOrder should add an order with id 3`() {
+    fun `createOrder should add a new order`() {
         //when
         val aKnife = Product(id = null, name = "knife", price = 25.0.toBigDecimal(), weight = 3.0.toBigDecimal())
         val aDesk = Product(id = null, name = "desk", price = 100.0.toBigDecimal(), weight = 20.0.toBigDecimal())
         val insertedOrder = orderDBServiceImpl.createOrder(listOf(aDesk, aKnife))
         val orders = orderDBServiceImpl.getAllOrders()
         //then
-        Assertions.assertThat(orders).hasSize(3)
-        Assertions.assertThat(insertedOrder.id).isEqualTo(3)
-        Assertions.assertThat(orders[2].status).isEqualTo(OrderStatus.PENDING)
+        assertThat(orders).hasSize(3)
+        assertThat(insertedOrder.id).isNotNull
+        assertThat(insertedOrder.status).isEqualTo(OrderStatus.PENDING)
+        assertThat(insertedOrder.products).hasSize(2)
     }
 }
